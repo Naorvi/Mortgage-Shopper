98e1429ef7fe576d26f060890321ddaafd64a5fe
Naorvi <55125540+Naorvi@users.noreply.github.com>
2020-03-13 18:38:12 -0400

    Update MortgageShopper.java

--- MortgageShopper.java
+++ MortgageShopper.java
@@ -10,7 +10,7 @@ class MortgageShopper {
         public static void main(String[] args) throws IOException {
                 Auto a=new Auto("Lo Yield",3,100,10,4.4);
                 double month=a.calculateMonthly();
-                System.out.println(a+"\nthis is the monthly payment "+month);
+                System.out.println(a+"\nThis is the monthly payment "+month);
                 GUI.createAndShowGUI();
         }
 }
