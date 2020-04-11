394b0d403c894bead94c17c9960dd03b90c153a6
smmk123 <smmk123@gmail.com>
2020-04-10 22:34:10 -0400

    added input validation

--- Controller.java
+++ Controller.java
@@ -1,6 +1,6 @@
+import com.mysql.cj.util.StringUtils;
 import mortgageShopper.Auto;
 import mortgageShopper.Business;
-import mortgageShopper.Loan;
 import mortgageShopper.Mortgage;
 
 import java.awt.*;
@@ -16,9 +16,29 @@ public class Controller {
         g.createAndShowGUI();
     }
 
+    public boolean isValid(){
+        try {
+            getPrinciple();
+            getDown();
+            return true;}
+        catch (Exception e) {
+            g.getErrorText().setText("Please enter a Valid Principle and Down Payment.");
+            return false;
+        }
+
+    }
+    public boolean isTermSelected(){
+        if (loanType==0||length==0){
+            g.getErrorText().setText("Please Select a loan type and Lona length.");
+            return false;
+        }
+        else return true;
+    }
+
     public String getName(){
         return String.valueOf(g.getName());
     }
+
     public double getPrinciple(){
         return Double.parseDouble(g.getPrinciple().getText());
     }
@@ -84,46 +104,50 @@ public class Controller {
 
         g.getYear3().addActionListener(e->length=36);
 
-        g.getSubmitButton().addActionListener(e->{
-            CardLayout cl = (CardLayout)(g.getCards().getLayout());
-            cl.show(g.getCards(), "RESULTS");
-           if(loanType==1){
-               if (length==36) {
-                   Auto a = new Auto("Auto 36", 36, getPrinciple(), getDown(), 5.0);
-                   double month=a.calculateMonthly();
-                   g.getResults().setText(a+"\nthis is the monthly payment "+month+"\n");
-               }
-               if(length==72){
-                   Auto a = new Auto("Auto 72", 72, getPrinciple(), getDown(), 5.0);
-                   double month=a.calculateMonthly();
-                   g.getResults().setText(a+"\nthis is the monthly payment "+month+"\n");
-               }
-           }
-            if(loanType == 2){
-                if (length==180) {
-                    Mortgage m = new Mortgage("Home 15 year", 180, getPrinciple(), getDown(), 5.0);
-                    double month=m.calculateMonthly();
-                    g.getResults().setText(m+"\nthis is the monthly payment "+month+"\n");
+        g.getSubmitButton().addActionListener(e-> {
+                    if (isValid() && isTermSelected()) {
+                        CardLayout cl = (CardLayout) (g.getCards().getLayout());
+                        cl.show(g.getCards(), "RESULTS");
+                        if (loanType == 1) {
+                            if (length == 36) {
+                                Auto a = new Auto("Auto 36", 36, getPrinciple(), getDown(), 5.0);
+                                double month = a.calculateMonthly();
+                                g.getResults().setText(a + "\nthis is the monthly payment " + month + "\n");
+                            }
+                            if (length == 72) {
+                                Auto a = new Auto("Auto 72", 72, getPrinciple(), getDown(), 5.0);
+                                double month = a.calculateMonthly();
+                                g.getResults().setText(a + "\nthis is the monthly payment " + month + "\n");
+                            }
+                        }
+                        if (loanType == 2) {
+                            if (length == 180) {
+                                Mortgage m = new Mortgage("Home 15 year", 180, getPrinciple(), getDown(), 5.0);
+                                double month = m.calculateMonthly();
+                                g.getResults().setText(m + "\nthis is the monthly payment " + month + "\n");
+                            }
+                            if (length == 360) {
+                                Mortgage m = new Mortgage("Home 30 year", 360, getPrinciple(), getDown(), 5.0);
+                                double month = m.calculateMonthly();
+                                g.getResults().setText(m + "\nthis is the monthly payment " + month + "\n");
+                            }
+                        }
+                        if (loanType == 3) {
+                            if (length == 60) {
+                                Business b = new Business("Business 5 year", 60, getPrinciple(), getDown(), 5.0);
+                                double month = b.calculateMonthly();
+                                g.getResults().setText(b + "\nthis is the monthly payment " + month + "\n");
+                            }
+                            if (length == 36) {
+                                Business b = new Business("Business 3 year", 36, getPrinciple(), getDown(), 5.0);
+                                double month = b.calculateMonthly();
+                                g.getResults().setText(b + "\nthis is the monthly payment " + month + "\n");
+                            }
+                        }
+                    }
                 }
-                if(length==360){
-                    Mortgage m = new Mortgage("Home 30 year", 360, getPrinciple(), getDown(), 5.0);
-                    double month=m.calculateMonthly();
-                    g.getResults().setText(m+"\nthis is the monthly payment "+month+"\n");
-                }
-            }
-            if(loanType == 3){
-                if (length==60) {
-                    Business b = new Business("Business 5 year", 60, getPrinciple(), getDown(), 5.0);
-                    double month=b.calculateMonthly();
-                    g.getResults().setText(b+"\nthis is the monthly payment "+month+"\n");
-                }
-                if(length==36){
-                    Business b = new Business("Business 3 year", 36, getPrinciple(), getDown(), 5.0);
-                    double month=b.calculateMonthly();
-                    g.getResults().setText(b+"\nthis is the monthly payment "+month+"\n");
-                }
-            }
-        });
+
+        );
     }
 
 
